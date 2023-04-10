b1ec295a453fbe2fb4bb3d13263daa84fbf29283
Yisak Girmay <yisakbruck23@gmail.com>
2023-04-09 23:25:05 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+const a = document.getElementById("main")
+a.remove()
+
+
+let newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+
+newHeader.innerHTML = "Yisak is the champion";
+
+document.body.appendChild(newHeader);
+
+
+
